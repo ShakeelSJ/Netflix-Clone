2d7273c4db6dcd82bb902ee4c740b8261e24e708
Shakeel Jarhejo <shakeel@nativebrains.com>
2021-04-05 12:54:27 +0500

    HomePage UI

--- lib/main.dart
+++ lib/main.dart
@@ -25,13 +25,23 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
+    int selectedIndex = 0;
+    PageController pageController = PageController();
+    void onTap(int pageValue) {
+      setState(() {
+        selectedIndex = pageValue;
+      });
+      pageController.jumpToPage(pageValue);
+    }
+
     return Scaffold(
+      backgroundColor: Colors.black,
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.black87,
         selectedItemColor: Colors.white,
         unselectedItemColor: Colors.white60,
-        selectedFontSize: 12,
-        unselectedFontSize: 12,
+        selectedFontSize: 12.0,
+        unselectedFontSize: 11.0,
         type: BottomNavigationBarType.fixed,
         items: [
           BottomNavigationBarItem(
@@ -53,6 +63,113 @@ class _HomepageState extends State<Homepage> {
           BottomNavigationBarItem(
             icon: Icon(Icons.more_horiz_outlined),
             label: 'More',
+          ),
+        ],
+        onTap: onTap,
+      ),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            backgroundColor: Colors.black,
+            leading: Image.asset('img/icon.png', scale: 20.0),
+            actions: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(right: 15.0),
+                child: MaterialButton(
+                  onPressed: () {},
+                  child: Text('TV Shows',
+                      style: TextStyle(color: Colors.white, fontSize: 18.0)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 15.0),
+                child: MaterialButton(
+                  onPressed: () {},
+                  child: Text('Movies',
+                      style: TextStyle(color: Colors.white, fontSize: 18.0)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 15.0),
+                child: MaterialButton(
+                  onPressed: () {},
+                  child: Text('My List',
+                      style: TextStyle(color: Colors.white, fontSize: 18.0)),
+                ),
+              )
+            ],
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              height: 250.0,
+              child: Image.asset('img/bg.jpg'),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 15.0),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    MaterialButton(
+                      onPressed: null,
+                      child: Column(
+                        children: <Widget>[
+                          Image.asset(
+                            'img/plus.png',
+                            scale: 22.0,
+                            color: Colors.white,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8.0),
+                            child: Text(
+                              'My List',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 18.0),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(5)),
+                      child: FlatButton.icon(
+                          onPressed: null,
+                          icon: Icon(
+                            Icons.play_arrow,
+                            color: Colors.black,
+                            size: 30.0,
+                          ),
+                          label: Text(
+                            'Play',
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 18.0),
+                          )),
+                    ),
+                    MaterialButton(
+                      onPressed: null,
+                      child: Column(
+                        children: <Widget>[
+                          Image.asset(
+                            'img/info.png',
+                            scale: 22.0,
+                            color: Colors.white,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8.0),
+                            child: Text(
+                              'Info',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 18.0),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ]),
+            ),
           ),
         ],
       ),
